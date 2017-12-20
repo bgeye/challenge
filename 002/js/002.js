36bb1eab8f3642cc67bb8777ff7c395ebfca8b75
Mario Knecht <make@logobject.ch>
2017-12-20 14:50:54 +0100

    remove console log

--- 002/js/002.js
+++ 002/js/002.js
@@ -23,19 +23,13 @@ function movebtn(){
     setTimeout(function(){
         crossLines();
     },500);
-
-
 }
 
 function crossLines(){
     var lines = document.querySelectorAll('.nav-btn__linetop, .nav-btn__linebottom');
-    console.log('lines to add openclass: '+ lines.length);
-    console.log(lines);
     for(var i = 0; i < lines.length;i++){
-            console.log('in loop');
 
             if((i + 1)==(lines.length)){
-                console.log('lastline');
                 var lastline = lines[i];
                 lines[i].addEventListener('transitionend',setOpenClass(lastline));
                 lines[i].className += ' rotate';
@@ -44,30 +38,20 @@ function crossLines(){
             console.log('first Line');
             lines[i].className += ' rotate';
             }
-
     }
-
-
-
 }
 
 function setOpenClass(line){
     var lines = line;
-    console.log('lines in openclass: '+lines);
     lines.removeEventListener('transitionend',setOpenClass);
-    console.log('transition ends here.');
 
     var btn = document.getElementsByClassName('nav-btn');
     btn[0].className += ' open';
     var openbtn = document.getElementsByClassName('open');
     openbtn[0].addEventListener('click',removeClass);
-    console.log('eventlistener set');
-
 }
 
 function removeClass(){
-    console.log('removeClass');
-
     var lines = document.getElementsByClassName('rotate');
     while(lines.length){
         lines[0].classList.remove('rotate');
@@ -79,8 +63,6 @@ function removeClass(){
             lines[0].classList.remove('move');
         }
     },500);
-
-
 
     var openbtn = document.getElementsByClassName('open');
     openbtn[0].removeEventListener('click',removeClass);
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 reference: https://100dayscss.com/
 
-001 - grid, flexbox, transform, box-shadow, gradient
-002 - grid, flexbox, transition
+001 - grid, flexbox, transform, box-shadow, gradient <br>
+002 - grid, flexbox, transition, box-shadow, javascript <br>
